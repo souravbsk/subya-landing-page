2ca143935cdfee94b5a078f0356ad6837100f87e
souravbsk <souravbsk01@gmail.com>
2024-01-22 19:52:21 +0600

    shop done

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -11,9 +11,9 @@ const ProductCard = ({ image, title, price, pera }) => {
           className="w-full h-full transition-transform transform "
         />
       </div>
-      <div className="p-4 text-center  absolute group-hover:opacity-100 w-full group-hover:bg-[#5D2F18]   opacity-0 transition-opacity top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h3 className="text-xl capitalize text-white font-bold mb-2">{title}</h3>
-        <p className="text-gray-100 mb-2">{pera}</p>
+      <div className="p-4 text-center  absolute group-hover:opacity-100 shopbg w-full group-hover:bg-[#5D2F18]   opacity-0 transition-opacity top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h3 className="text-xl capitalize text-white font-bold ">{title}</h3>
+        {/* <p className="text-gray-100 mb-2">{pera}</p> */}
         {/* <p className="text-[#7A5542] text-xl font-bold">${price}</p> */}
       </div>
     </div>
