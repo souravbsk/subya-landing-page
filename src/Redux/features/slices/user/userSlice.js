2094dbe02f33483daa35e1053c8090e8788daac6
souravbsk <souravbsk01@gmail.com>
2024-02-14 16:36:03 +0600

    login api done

--- src/Redux/features/slices/user/userSlice.js
+++ src/Redux/features/slices/user/userSlice.js
@@ -1,46 +1,63 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import {
+  GoogleAuthProvider,
   createUserWithEmailAndPassword,
-  updatePhoneNumber,
+  signInWithEmailAndPassword,
+  signInWithPopup,
   updateProfile,
 } from "firebase/auth";
 import auth from "../../../../Utility/firebase.init";
 
 const initialState = {
-  name: "",
-  email: "",
+  user: null,
   isLoading: false,
   isError: false,
   error: "",
 };
+
+// create new user
 export const createUser = createAsyncThunk(
   "users/createUser",
   async ({ email, password, name }) => {
-  
-      console.log(email, password);
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      await updateProfile(auth.currentUser, { displayName: name });
-      // await updatePhoneNumber(auth.currentUser, phone);
-      console.log(userCredential, "user credential ");
-      const newUser = {
-        email: userCredential.user.email,
-        name: userCredential.user.displayName,
-      };
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    await updateProfile(auth.currentUser, { displayName: name });
 
-      return newUser;
-  
+    return userCredential.user;
+  }
+);
+
+// login user
+export const loginUser = createAsyncThunk(
+  "users/loginUser",
+  async ({ email, password }) => {
+    const userData = await signInWithEmailAndPassword(auth, email, password);
+    return userData?.user;
+  }
+);
+
+//logout user
+
+export const signInWithGoogle = createAsyncThunk(
+  "users/signInWithGoogle",
+  async () => {
+    const googleProvider = new GoogleAuthProvider();
+    const userData = await signInWithPopup(auth, googleProvider);
+    return userData?.user;
   }
 );
 
 const userSlice = createSlice({
   name: "userSlice",
   initialState,
-  reducers: {},
+  reducers: {
+    setLo,
+  },
   extraReducers: (builder) => {
+    //register chain
     builder
       .addCase(createUser.pending, (state) => {
         state.isLoading = true;
@@ -50,10 +67,30 @@ const userSlice = createSlice({
 
       .addCase(createUser.fulfilled, (state, { payload }) => {
         state.isLoading = false;
-        state.name = payload.name;
-        state.email = payload.email;
+        state.user = payload;
+        state.isError = false;
+        state.error = "";
       })
       .addCase(createUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.error = action.error.message;
+      });
+
+    //login chain
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(loginUser.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.user = payload;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(loginUser.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.error = action.error.message;
